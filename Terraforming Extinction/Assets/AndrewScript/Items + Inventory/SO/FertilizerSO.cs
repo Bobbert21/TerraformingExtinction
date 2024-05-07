a4578943d8dbe3d058cc156f4aa3eb21ec918fcf
AndJHu97 <Andrewhu1997@gmail.com>
2024-05-07 18:28:48 -0500

    Fertilizer on uprooters and uprooterSO

--- Terraforming Extinction/Assets/AndrewScript/Items + Inventory/SO/FertilizerSO.cs
+++ Terraforming Extinction/Assets/AndrewScript/Items + Inventory/SO/FertilizerSO.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum FertilizerTypes
+{
+    Red,
+    Blue,
+    White,
+    Yellow,
+    Green,
+    Count
+}
+
 [CreateAssetMenu(fileName = "FertilizerSO", menuName = "ScriptableObject/Fertilizers")]
 public class FertilizerSO : ItemSO
 {
-    public enum FertilizerTypes
-    {
-        Red,
-        Blue,
-        White,
-        Yellow,
-        Green
-    }
+   
 
     public FertilizerTypes FertilizerType;
 }
